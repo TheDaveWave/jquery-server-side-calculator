dd36a4083579b41d63c4fcbe2c44e61c2d618948
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-09 16:24:26 -0500

    got the operator based on the event click

--- server/public/client.js
+++ server/public/client.js
@@ -2,5 +2,14 @@
 $(readyNow);
 
 function readyNow () {
-    
+    $('button').on('click', getOperator);
+}
+
+// store operator into variable
+let operator = '';
+
+// function to get the operator from the corresponding button.
+function getOperator (event) {
+    operator = $(event.target).text()
+    console.log(operator);
 }
