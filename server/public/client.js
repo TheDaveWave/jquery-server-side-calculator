61b676fdc4d231c72df393c4cd98bcf2e3a7eb29
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-09 16:38:06 -0500

    assigned class operator to buttons with math operators

--- server/public/client.js
+++ server/public/client.js
@@ -2,14 +2,22 @@
 $(readyNow);
 
 function readyNow () {
-    $('button').on('click', getOperator);
+    $('.operator').on('click', getOperation);
 }
 
 // store operator into variable
-let operator = '';
+let operation = {};
 
 // function to get the operator from the corresponding button.
-function getOperator (event) {
-    operator = $(event.target).text()
-    console.log(operator);
+function getOperation (event) {
+    operation.num1 = $('#num1').val();
+    operation.num2 = $('#num2').val();
+    operation.operator = $(event.target).text();
+    console.log(operation);
+
+    // empty inputs
+    $('#num1').val('');
+    $('#num2').val('');
 }
+
+// function to send mathmatical operations to server.
