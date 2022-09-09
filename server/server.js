293a81aad00f19021d0b6472114e22d2ad7ca3ed
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-09 15:47:09 -0500

    set up a port variable

--- server/server.js
+++ server/server.js
@@ -3,3 +3,8 @@ const express = require('express');
 const app = express();
 // set a port to host local server.
 const PORT = 5000;
+
+
+app.use(express.static('server/public'));
+// enable requests?
+app.use(express.urlencoded({extended : true}));
