73325ed2cede798dce7e97d58bb43c4b47b0f143
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-13 09:30:18 -0500

    added environment variable for port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 // set a port to host local server.
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(express.static('server/public'));
